654bd02ba8b5a2b2621917480f3236f17408655f
Nasserine B <benamaranasserine@gmail.com>
2022-09-13 09:40:15 +0200

    create survey table and migration

--- BigScreen_API/database/migrations/2022_09_12_114959_create_survey_table.php
+++ BigScreen_API/database/migrations/2022_09_12_114959_create_survey_table.php
@@ -15,25 +15,26 @@ return new class extends Migration
     {
         Schema::create('survey', function (Blueprint $table) {
             $table->id();
-            // $table->string('email', 50)->nullable(false);
-            // $table->string('age')->nullable(false);
-            // $table->string('gender')->nullable(false);
-            // $table->string('nbPersonFamily')->nullable(false);
-            // $table->string('job')->nullable(false);
-            // $table->string('vrBrand')->nullable(false);
-            // $table->string('storeBuyVr')->nullable(false);
-            // $table->string('helmetBuyInFuture')->nullable(false);
-            // $table->string('nbPeopleInFamilyUseVrForBigScreen')->nullable(false);
-            // $table->string('mainlyUseBigscrrenFor')->nullable(false);
-            // $table->string('pointsForImageQuality')->nullable(false);
-            // $table->string('pointForComfortInterface')->nullable(false);
-            // $table->string('pointForQuality3Dgraphics')->nullable(false);
-            // $table->string('pointForAudioQuality')->nullable(false);
-            // $table->string('preciseNotifications')->nullable(false);
-            // $table->string('inviteFriendJoin')->nullable(false);
-            // $table->string('recordTVshows')->nullable(false);
-            // $table->string('playExclusiveGames')->nullable(false);
-            // $table->string('newFeature')->nullable(false);
+            $table->string('responseOne')->nullable(false)->unique();
+            $table->string('responseTwo')->nullable(false);
+            $table->string('responseThree')->nullable(false);
+            $table->string('responseFour')->nullable(false);
+            $table->string('responseFive')->nullable(false);
+            $table->string('responseSix')->nullable(false);
+            $table->string('responseSeven')->nullable(false);
+            $table->string('responseEight')->nullable(false);
+            $table->string('responseNine')->nullable(false);
+            $table->string('responseTen')->nullable(false);
+            $table->string('responseEleven')->nullable(false);
+            $table->string('responseTwelve')->nullable(false);
+            $table->string('responseThirteen')->nullable(false);
+            $table->string('responseFourteen')->nullable(false);
+            $table->string('responseFifteen')->nullable(false);
+            $table->string('responseSixteen')->nullable(false);
+            $table->string('responseSeventeen')->nullable(false);
+            $table->string('responseEighteen')->nullable(false);
+            $table->string('responseNineteen')->nullable(false);
+            $table->string('responseTwenty')->nullable(false);
             $table->timestamps();
         });
     }
