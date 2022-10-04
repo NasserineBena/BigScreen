d1aa828f99af60e14387e90338f0ac118dc15b91
nnota <tramm1006@gmail.com>
2022-10-04 09:58:12 +0200

    Ajouter validation formulaire par laravel et ajouter deconnexion button

--- BigScreen_API/app/Http/Controllers/API/SurveyUserController.php
+++ BigScreen_API/app/Http/Controllers/API/SurveyUserController.php
@@ -29,12 +29,65 @@ class SurveyUserController extends Controller
      */
     public function store(Request $request)
     {
-        $faker= app(Generator::class);
-        $surveyUser = new SurveyUser;
-        $surveyUser->email = $request->email;
-        $surveyUser->token= $faker->regexify('[A-Za-z0-9]{20}');
-        $surveyUser->save();
-        return response()->json($surveyUser);
+        $data = json_decode($request['request'], true);
+        foreach($data as $mydata)
+        {
+            if($mydata==""){
+                return response()->json("Tous les champs doivent être remplis");
+            }
+
+        }
+        if(filter_var($data[1], FILTER_VALIDATE_EMAIL)){
+            // return is_numeric($data[2]);
+            if(is_numeric($data[2]) && (int)$data[2]<100 && (int)$data[2]>5){
+                $a =  SurveyUser::where('email','=', $data[1]) -> first();
+                // return "true";
+                if(!$a){
+                    $faker= app(Generator::class);
+                    $surveyUser = new SurveyUser;
+                    $surveyUser->email = $data[1];
+                    $surveyUser->token= $faker->regexify('[A-Za-z0-9]{20}');
+                    $surveyUser->save();
+                    return response()->json($surveyUser);
+                }
+                else{
+                    return response()->json("Cet email existe déjà");
+                }
+            }
+            else{
+                return response()->json("L'âge doit être numeric et plus grand que 5 ans ");
+            }
+        }
+        else{
+            return response()->json("Cet email n'est pas au bon format");
+        }
+        
+        // $faker= app(Generator::class);
+        // $surveyUser = new SurveyUser;
+        // $surveyUser->email = $request->email;
+        // $surveyUser->token= $faker->regexify('[A-Za-z0-9]{20}');
+        // $surveyUser->save();
+        // return response()->json($surveyUser);
+        // $faker= app(Generator::class);
+        // $validator = $this->validate($request,[
+        //      'email' => 'required|regex:/(.+)@(.+)\.(.+)/i',]);
+        // $validator = \Validator::make($request, [
+        //     'email' => 'required|email'])
+        // $this->validate($request, [
+        //     "1" => 'required|email'
+        // ]);
+        // if(is_numeric($request->2)){
+        //     return response()->json(true)
+        // }
+        // // return response()->json($validate);
+        // $faker= app(Generator::class);
+        // $surveyUser = new SurveyUser;
+        // $surveyUser->email = $request->email;
+        // $surveyUser->token= $faker->regexify('[A-Za-z0-9]{20}');
+        // $surveyUser->save();
+       
+        
+        
     }
 
     /**
--- bigScreenVueFront/src/components/admin/NavBar.vue
+++ bigScreenVueFront/src/components/admin/NavBar.vue
@@ -7,10 +7,45 @@
         <a href="/admin/questionnaire">Questionnaire</a>
       </div>
       <div class="padding"><a href="/admin/reponses">Réponses</a></div>
+      <div v-on:click.prevent="logout" class="padding"><a href="/admin/reponses">Déconnexion</a></div>
     </div>
   </div>
 </template>
+<script>
+import axios from "axios";
+import { RouterLink, RouterView } from "vue-router";
+import { useRouter } from "vue-router";
 
+export default {
+  data() {
+    return {
+      connexion: false,
+      urlApi: import.meta.env.VITE_URL_API,
+    };
+  },
+  methods: {
+    logout() {
+      let token = localStorage.getItem("access_token");
+      axios
+        .post(this.urlApi+"logout", null, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        })
+        .then(({ data }) => {
+          this.$router.push("/login");
+          this.connexion = false;
+          localStorage.removeItem("nameAdmin");
+          localStorage.removeItem("access_token");
+        })
+        .catch((error) => {
+          window.alert("Vous ne pouvez pas se déconnecter ");
+        });
+    },   
+  },
+};
+</script>
 <style scoped>
 .container {
   width: fit-content;
